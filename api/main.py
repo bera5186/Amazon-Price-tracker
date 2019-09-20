6ae1a062142280deb8049490b06094f70e904c01
bera5186 <rbasu611@gmail.com>
2019-09-20 21:19:59 +0530

    Created API endpoint for fetching username

--- api/main.py
+++ api/main.py
@@ -16,13 +16,13 @@ api = Api(app)
 
 # Database Settings
 try:
-    conn = MongoClient('mongodb://localhost:27017')
-    db   = conn.amazon_price_tracker
+    conn = MongoClient("mongodb://localhost:27017")
+    db = conn.amazon_price_tracker
     productCollection = db.products
     usersCollection = db.users
-    print('db connected')
+    print("db connected")
 except:
-    print('error connecting to db')
+    print("error connecting to db")
 
 
 class CreateUser(Resource):
@@ -37,49 +37,52 @@ class CreateUser(Resource):
         userName = postedData["username"]
         password = postedData["password"]
 
-        checkedEmail = usersCollection.find_one({'email' : email})
+        checkedEmail = usersCollection.find_one({"email": email})
 
         if checkedEmail is None:
-            hashedPassword = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt(8))
+            hashedPassword = bcrypt.hashpw(password.encode("utf-8"), bcrypt.gensalt(8))
 
             userDocument = {
-                "email" : email,
-                "password" : hashedPassword,
-                "username" : userName
-
+                "email": email,
+                "password": hashedPassword,
+                "username": userName,
             }
 
             usersCollection.insert_one(userDocument)
 
             try:
-                js = json.dumps({
-                    "message" : "User succesfully created",
-                    "success" : True
-                })
-                response = Response(js, status=201, mimetype='application/json')
+                js = json.dumps(
+                    {"message": "User succesfully created", "success": True}
+                )
+                response = Response(js, status=201, mimetype="application/json")
             except:
-                js = json.dumps({
-                    "message" : "Cannot create a user",
-                    "success" : False
-                })
-                response = Response(js, status=500, mimetype='application/json')
+                js = json.dumps({"message": "Cannot create a user", "success": False})
+                response = Response(js, status=500, mimetype="application/json")
 
             return response
         else:
-            js = json.dumps({
-                "message" : "Email already taken",
-                "success" : False
-            })
+            js = json.dumps({"message": "Email already taken", "success": False})
 
-            response = Response(js, status=400, mimetype='application/json')
+            response = Response(js, status=400, mimetype="application/json")
             return response
-        
-
-        
-        
 
 
-class GetUser(Resource):
+class Sum(Resource):
+    def get(self):
+
+        postedData = request.get_json()
+        x = postedData["x"]
+        y = postedData["y"]
+
+        sum = int(x) + int(y)
+
+        js = json.dumps({"sum": sum})
+
+        response = Response(js, status=200, mimetype="application/json")
+        return response
+
+
+class GetUserForLogin(Resource):
     def get(self):
         """
         Check for a user in database
@@ -90,44 +93,75 @@ class GetUser(Resource):
         email = postedData["email"]
         password = postedData["password"]
 
-        dbEmail = usersCollection.find_one({'email': email})
+        dbEmail = usersCollection.find_one({"email": email})
 
         if dbEmail is None:
-            js = json.dumps({
-                "message" : "Incorrect Email or Password",
-                "success" : False
-            })
+            js = json.dumps(
+                {"message": "Incorrect Email or Password", "success": False}
+            )
 
-            reponse = Response(js, status=404, mimetype='application/json')
+            reponse = Response(js, status=404, mimetype="application/json")
             return reponse
         else:
-            if bcrypt.hashpw(password.encode('utf-8'), dbEmail['password']) == dbEmail['password']:
-                js = json.dumps({
-                    "message" : "successfully logged in",
-                    "sucess" : True
-                })
+            if (
+                bcrypt.hashpw(password.encode("utf-8"), dbEmail["password"])
+                == dbEmail["password"]
+            ):
+                js = json.dumps({"message": "successfully logged in", "sucess": True})
 
-                response = Response(js, status=200, mimetype='application/json')
+                response = Response(js, status=200, mimetype="application/json")
                 return response
 
             else:
-                js = json.dumps({
-                    "message" : "Incorrect email or password",
-                    "sucess " : False
-                })
+                js = json.dumps(
+                    {"message": "Incorrect email or password", "sucess ": False}
+                )
 
-                response = Response(js, status=404, mimetype='application/json')
+                response = Response(js, status=404, mimetype="application/json")
                 return response
 
 
+class GetUserName(Resource):
+    def get(self):
+        """
+            API endpoint to get a single user
+        
+        """
+
+        postedData = request.get_json()
+        email = postedData["email"]
+
+        user = usersCollection.find_one({"email": email})
+
+        if user is None:
+            js = json.dumps(
+                {
+                    "message": "User with email" + email + " not found in database",
+                    "success": False,
+                }
+            )
+
+            response = Response(js, status=404, mimetype="application/json")
+            return response
+
+        else:
+
+            js = json.dumps({"data": user["username"], "success": True})
+
+            response = Response(js, status=200, mimetype="application/json")
+            return response
+
+
 api.add_resource(CreateUser, "/signup")
-api.add_resource(GetUser, "/signin")
+api.add_resource(GetUserForLogin, "/signin")
+api.add_resource(GetUserName, "/getuser")
+api.add_resource(Sum, "/sum")
 
 
-@app.route('/')
+@app.route("/")
 def home():
     return "hello world"
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(debug=True)
